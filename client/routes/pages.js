29745635516ede497b858ca93066a7c524f3177b
Lee Jones KE <93858479+Leejoneske@users.noreply.github.com>
2025-04-23 16:26:06 +0300

    Update pages.js

--- client/routes/pages.js
+++ client/routes/pages.js
@@ -1,1 +1,26 @@
 
+const express = require('express');
+const router = express.Router();
+const pageController = require('../controllers/pageController');
+const languageMiddleware = require('../middlewares/language');
+
+router.use(languageMiddleware);
+
+// Main pages
+router.get('/', pageController.renderPage('index'));
+router.get('/sell', pageController.renderPage('sell'));
+router.get('/history', pageController.renderPage('history'));
+router.get('/about', pageController.renderPage('about'));
+router.get('/referral', pageController.renderPage('referral'));
+
+// Language switcher
+router.post('/set-language', (req, res) => {
+    const { language } = req.body;
+    if (['en', 'ru'].includes(language)) {
+        res.cookie('lang', language, { maxAge: 900000, httpOnly: true });
+        req.language = language;
+    }
+    res.redirect('back');
+});
+
+module.exports = router;
