04b058aaf488970b5ff603b13853bf373179f081
Lee Jones KE <93858479+Leejoneske@users.noreply.github.com>
2025-04-23 16:25:09 +0300

    Update pageController.js

--- client/controllers/pageController.js
+++ client/controllers/pageController.js
@@ -1,1 +1,31 @@
 
+const path = require('path');
+const { getTranslations } = require('../config/i18n');
+
+exports.renderPage = (pageName) => {
+    return (req, res) => {
+        // Inject translations into the page
+        const translations = getTranslations(req.language);
+        
+        res.render(pageName, {
+            translations,
+            currentLanguage: req.language,
+            user: req.user // If authenticated
+        });
+    };
+};
+
+exports.handle404 = (req, res) => {
+    res.status(404).render('errors/404', {
+        translations: getTranslations(req.language),
+        currentLanguage: req.language
+    });
+};
+
+exports.handle500 = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).render('errors/500', {
+        translations: getTranslations(req.language),
+        currentLanguage: req.language
+    });
+};
